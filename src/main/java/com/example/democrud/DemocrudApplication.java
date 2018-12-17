10b5a04744ad592ffa80c16ef7d7938fd87a3358
ChrisLi <top770977@163.com>
2018-12-17 18:50:22 +0800

    20181217

--- src/main/java/com/example/democrud/DemocrudApplication.java
+++ src/main/java/com/example/democrud/DemocrudApplication.java
@@ -1,12 +1,33 @@
 package com.example.democrud;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DemocrudApplication {
+//@SpringBootApplication
+//public class DemocrudApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(DemocrudApplication.class, args);
+//	}
+//}
 
+/**
+ *
+ * Title: Application
+ * Description:
+ * springBoot 主程序
+ * Version:1.0.0
+ * @author pancm
+ * @date 2018年1月5日
+ */
+
+@SpringBootApplication
+@MapperScan("com.example.democrud.dao")
+public class DemocrudApplication {
 	public static void main(String[] args) {
+		// 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
 		SpringApplication.run(DemocrudApplication.class, args);
+		System.out.println("程序正在运行...");
 	}
 }
